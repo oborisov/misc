b21a8060135c4559c4afaf076396469e200bb4c9
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2023-08-10 07:58:59 +0200

    Write to file, add timestamp

--- 20230810_Firth/Firth_test.R
+++ 20230810_Firth/Firth_test.R
@@ -1,5 +1,12 @@
+# conda activate logistf
 library(brglm2)
 library(logistf)
+# print
+timestamp <- format(Sys.time(), format = "%Y%m%d_%H%M%S")
+fileConn_name <- paste0(timestamp, "_Firth_log.txt")
+fileConn <- file(fileConn_name, "w")
+sink(fileConn)
+# code
 test_df <- data.frame(phenotype=c(rep(1, 80), rep(0, 1900), rep(1, 20)),
                       SNP=c(rep(0, 1900), rep(1, 100)))
 table(test_df)
@@ -8,3 +15,5 @@ summary(glm(phenotype ~ SNP, data = test_df, family = binomial))
 summary(glm(phenotype ~ SNP, family = binomial(logit), data = test_df, method = "brglmFit", type = "AS_mean"))
 summary(logistf::logistf(phenotype ~ SNP, data=test_df))
 
+# close connectoin
+sink(); close(fileConn)
