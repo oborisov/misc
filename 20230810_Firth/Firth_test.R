f20a347c27a1af266186dc6e1ec2e15bdd89204c
Oleg Borisov <oleg.borisov@uniklinik-freiburg.de>
2023-08-10 09:09:35 +0200

    Add running time

--- 20230810_Firth/Firth_test.R
+++ 20230810_Firth/Firth_test.R
@@ -1,4 +1,3 @@
-# conda activate logistf
 library(brglm2)
 library(logistf)
 # print
@@ -7,14 +6,18 @@ fileConn_name <- paste0(timestamp, "_Firth_log.txt")
 fileConn <- file(fileConn_name, "w")
 sink(fileConn)
 # code
-test_df <- data.frame(phenotype=c(rep(1, 80), rep(0, 1900), rep(1, 20)),
-                      SNP=c(rep(0, 1900), rep(1, 100)))
+test_df <- data.frame(phenotype=c(rep(1, 8000), rep(0, 190000), rep(1, 2000)),
+                      SNP=c(rep(0, 190000), rep(1, 10000)))
 head(test_df)
 table(test_df)
-fisher.test(as.matrix(table(test_df)))
-summary(glm(phenotype ~ SNP, data = test_df, family = binomial))
-summary(glm(phenotype ~ SNP, family = binomial(logit), data = test_df, method = "brglmFit", type = "AS_mean"))
-summary(logistf::logistf(phenotype ~ SNP, data=test_df))
+print(" ####### fisher.test ######## ")
+system.time(fisher.test(as.matrix(table(test_df))))
+print(" ####### Standard logistic regression ######## ")
+system.time(summary(glm(phenotype ~ SNP, data = test_df, family = binomial)))
+print(" ####### brglmFit, type = AS_mean ######## ")
+system.time(summary(glm(phenotype ~ SNP, family = binomial(logit), data = test_df, method = "brglmFit", type = "AS_mean")))
+print(" ####### logistf ######## ")
+system.time(summary(logistf::logistf(phenotype ~ SNP, data=test_df)))
 
 # close connectoin
 sink(); close(fileConn)
